6563b16100d524833c3d8960656a147a9865db79
unknown <UFG@CC6-07.ufg.local>
2024-02-06 18:52:08 -0600

    initial project

---
+++ app/src/main/java/sv/edu/fis/amd/angie/espinoza0602/MainActivity.kt
@@ -0,0 +1,11 @@
+package sv.edu.fis.amd.angie.espinoza0602
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
